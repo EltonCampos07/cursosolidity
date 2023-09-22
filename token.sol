5dcd1170025a463d718a7c94c5d22bc9f4bbe2a7
EltonCampos07 <eltonapcnunes@gmail.com>
2023-09-22 16:16:19 -0300

    feat: criação do arquivo token.sol

---
+++ token.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.8.19;
+
+import "contracts/bradesco_token_aberto.sol";
+
+contract Token is ExercicioToken {
+
+    ExercicioToken public contractAddress;
+
+    struct Client{
+       address _client;
+       uint256 _tokenBalance; 
+    }
+
+    mapping (address => Client) public client;
+
+    constructor() {
+        contractAddress = ExercicioToken(0x89A2E711b2246B586E51f579676BE2381441A0d0);
+    }
+
+    function MeuSaldo() public view returns (uint256) {
+        return contractAddress.balanceOf(msg.sender);
+    }
+
+    function GerarTokenParaEuCliente(uint256 amount) public onlyOwner {
+        require(contractAddress.mint(msg.sender, amount), "Token transfer failed.");
+        client[msg.sender]._tokenBalance += amount;
+    }
+
+}
